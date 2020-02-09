c095c5c40c1b4c624661a3a0fa7e1af1818882a3
paraz <mail2paras.s@gmail.com>
2020-02-09 18:05:03 +0530

    [UPDATE] example

--- example/__main__.py
+++ example/__main__.py
@@ -1,20 +1,54 @@
-from pulumi import log
-from lambda_packaging import LambdaPackaging, LambdaLayerPackaging
-
-lambda_package = LambdaPackaging(
-    'example-test', layer=True, exclude=['__main__.py'])
-
-lambda_layer = LambdaLayerPackaging('example-layer-test',
-                                    runtimes=['python3.6'],
-                                    layer_name="example-layer",
-                                    description="Example Layer for Testing",
-                                    exclude=[".gitignore", '__main__.py'])
+import pulumi
+from lambda_packaging import LambdaPackage, LambdaLayerPackage
+from pulumi_aws import lambda_, iam
+import json
 
 
-# path of package archive 
-# and lambda layer containing installed requirements
-log.info(lambda_package.package_archive)
-log.info(lambda_package.lambda_layer.arn)
+lambda_package = LambdaPackage(
+    name='example-test',
+    no_deploy=['pulumi', 'pulumi_aws', 'pulumi_docker'],
+    exclude=['__main__.py']
+)
 
-# lambda layer arn
-log.info(lambda_layer.lambda_layer.arn)
+role = iam.Role(
+    resource_name='role',
+    description=f'Role used by Lambda to run the `{pulumi.get_project()}-{pulumi.get_stack()}` project',
+    assume_role_policy=json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": [
+                    "lambda.amazonaws.com"
+                ]
+            },
+        }]
+    }),
+)
+
+# Attach the basic Lambda execution policy to our Role
+iam.RolePolicyAttachment(
+    resource_name='policy-attachment',
+    policy_arn='arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+    role=role.name,
+)
+
+
+# Create Lambda function
+function = lambda_.Function(
+    resource_name='function',
+    role=role.arn,
+    runtime='python3.6',
+    description=f'Lambda function running the f`{pulumi.get_project()}` ({pulumi.get_stack()}) project',
+    handler='handler.lambda_handler',
+    code=lambda_package.package_archive
+)
+
+# path of package archive and lambda layer
+# containing installed requirements
+pulumi.export("package_archive_path", lambda_package.package_archive)
+pulumi.export("layer_archive_path", lambda_package.layer_archive_path)
+
+pulumi.export("lambda_function", function.name)
